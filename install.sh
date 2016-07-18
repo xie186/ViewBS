17844ea6dd59edf74e1ace49d1318862f1c6c7ea
zeamxie <xieshaojun0621@qq.com>
2016-07-17 22:29:21 -0400

    add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/miyagawa/cpanminus/master/cpanm
+chmod 755 cpanm
+./cpanm Getopt::Long::Subcommand
+./cpanm Bio::DB::HTS::Tabix
+
+
+git clone https://github.com/samtools/htslib.git
+cd htslib
+utoheader     # If using configure, generate the header template...
+autoconf       # ...and configure script (or use autoreconf to do both)
+./configure    # Optional, needed for choosing optional functionality
+make
+make install
+
+
+echo "Checking R packages";
+R --vanilla --slave < lib/scripts/install_R_packages.R 
--- lib/scripts/install_R_packages.R
+++ lib/scripts/install_R_packages.R
@@ -1,1 +1,12 @@
+cat("Loading R libraries")
 
+if(!suppressMessages(require(ShortRead, warn.conflicts=F))) {
+    source("http://bioconductor.org/biocLite.R")
+    biocLite(ShortRead)
+    if(!suppressMessages(require(ShortRead, warn.conflicts=F))) {
+        stop('Loading package ShortRead failed!')
+    }
+}
+
+install.packages("ggplot2", dep=T)
+install.packages("pheatmap", dep=T)
