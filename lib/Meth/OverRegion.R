9bd15b9274c4a1b4a57d19a0855349a96ff2a2c5
zeamxie <xieshaojun0621@qq.com>
2016-07-20 23:43:16 -0400

    update README.md

--- lib/Meth/OverRegion.R
+++ lib/Meth/OverRegion.R
@@ -1,28 +1,37 @@
-methGeno <- function(meth, out){
-
-    library(ggplot2)
-    tab<-read.table(meth,head=T);
-     tab <- tab[order(tab[,3]),]
-
-    p=ggplot(tab, aes(x=bin_num, y=Methylation_level, group=sample_name, col=sample_name)) +
-	     geom_line()
-    #p = p + scale_x_continuous(breaks=c(min(tab$Pos), 0.5,max(tab$Pos)+min(tab$Pos), max(tab$Pos)), labels=c(abs(min(tab$Pos))/10, "TSS", "TTS", abs(min(tab$Pos))/10))
- 
-    p = p + scale_x_continuous(breaks=c(0.5,61.5), labels=c("TSS", "TTS"))
-     
-    ggsave(out, p)
-}
-
 Args <- commandArgs();
 
-cat("Usage: R --vanilla --slave --input <input tab> --flank <flanking regions> --tts <bin number> --output <output> < *R", "\n")
+cat("Usage: R --vanilla --slave --input <input tab> --xlab <Gene> --flank <flanking regions> --tts <bin number> --output <output> < *R", "\n")
 
 cat(Args, "\n")
 
+xlab = "Gene"
+
 for(i in 1:length(Args)){
-    if(Args[i] == "--input")   input  = Args[i+1]
-    if(Args[i] == "--tts")   input  = Args[i+1]
+    if(Args[i] == "--input")   meth = Args[i+1]
+    if(Args[i] == "--tts")     tss  = Args[i+1]
+    if(Args[i] == "--xlab")     xlab  = Args[i+1]
     if(Args[i] == "--output")  fig = Args[i+1]
 }
 
-methGeno(input, fig);
+
+library(ggplot2)
+tab<-read.table(meth,head=T);
+tab <- tab[order(tab[,3]),]
+
+p=ggplot(tab, aes(x=bin_num, y=Methylation_level, group=sample_name, col=sample_name)) +
+    geom_line() + xlab(xlab)
+    
+min <- min(tab$bin_num) -1
+max <- abs(max(tab$bin_num))
+    #p = p + scale_x_continuous(breaks=c(min(tab$Pos), 0.5,max(tab$Pos)+min(tab$Pos), max(tab$Pos)), labels=c(abs(min(tab$Pos))/10, "TSS", "TTS", abs(min(tab$Pos))/10))
+
+    #p = p + scale_x_continuous(breaks=c(min/2, (max + min)/2, max + min/2), labels=c("Upstream", xlab, "Downstream"))
+#p = p + scale_x_continuous(breaks=c(min/2, (max + min)/2, max + min/2), labels=c("Upstream", xlab, "Downstream"))
+#p = p + theme(axis.text.x = element_blank())
+
+flank = paste( -min/10, "kb", sep = " ")
+
+p = p + scale_x_continuous(breaks=c(min, max), labels=c(flank, flank));
+p = p + geom_vline(xintercept = c(0.5, max + min + 1.5), linetype = "dashed")
+ggsave(fig, p)
+
