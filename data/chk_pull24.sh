8aa4b78e950d6603ea88c22450932c65d060db1d
readbio <xieshaojun0621@qq.com>
2018-11-29 06:51:27 -0500

    Test pull24: Done!

---
+++ data/chk_pull24.sh
@@ -0,0 +1,2 @@
+### For version 0.1.5, https://github.com/xie186/ViewBS/pull/24. This is to test the bug. 
+ViewBS_v0.1.5/ViewBS MethHeatmap --region chk_pull24.tab --sample ../../data/WGBS_v4/wgbsview_05mm_3context_dep1.tab.gz,mm0.5 --sample ../../data/WGBS_v4/wgbsview_2mm_3context_dep1.tab.gz,mm2 --prefix test --context CHH
