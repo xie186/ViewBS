0c90d4508ff826915183b7b981c7fadd8618fc35
xie186 <xie186@purdue.edu>
2019-01-22 22:36:22 -0500

    Update

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -2,10 +2,10 @@
 PKG_NAME=viewbs
 USER=xie186
 
-#CONDA_UPLOAD_TOKEN=5c2fbd4489a6f38805b0003e
+CONDA_UPLOAD_TOKEN=5c2fbd4489a6f38805b0003e
 OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
-conda config --set anaconda_upload no
+conda config --set anaconda_upload yes
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 conda build .
