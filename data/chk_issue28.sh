01d9332603a153837ef6151cdb2f9d8bca7d76f3
xie186 <xie186@purdue.edu>
2019-02-26 08:36:40 -0500

    Update data/chk_issue28.sh

---
+++ data/chk_issue28.sh
@@ -0,0 +1,3 @@
+./memusg perl $Path2ViewBS/ViewBS MethCoverage --reference ../testdata/TAIR10_chr_all.fasta --sample bis_WT.tab.gz,WT  --outdir MethCoverage_test --prefix cmt2_proj_allsam_test
+
+## Note repeated
